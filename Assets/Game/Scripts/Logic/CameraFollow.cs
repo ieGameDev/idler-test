7c044329be0c853298b08deee09424ce35e1f666
Igor Eliseev <ie.game.develop@gmail.com>
2025-03-05 20:53:18 +0400

    camera following

---
+++ Assets/Game/Scripts/Logic/CameraFollow.cs
@@ -0,0 +1,48 @@
+using System;
+using Game.Scripts.Player;
+using UnityEngine;
+
+namespace Game.Scripts.Logic
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private float _distance;
+        [SerializeField] private float _offSetY;
+        [SerializeField] private float _rotationAngleX;
+        [SerializeField] private float _smoothTime;
+        
+        private Transform _target;
+        private Vector3 _currentVelocity;
+
+        private void Start()
+        {
+            _target = FindAnyObjectByType<PlayerMovement>().transform;
+        }
+
+        private void LateUpdate()
+        {
+            if (!_target) return;
+            CameraFollowing();
+        }
+        
+        // public void Follow(GameObject target) => 
+        //     _target = target.transform;
+
+        private void CameraFollowing()
+        {
+            Quaternion rotation = Quaternion.Euler(_rotationAngleX, 0, 0);
+            Vector3 position = rotation * new Vector3(0, 0, -_distance) + FollowingPointPosition();
+            
+            transform.position =
+                Vector3.SmoothDamp(transform.position, position, ref _currentVelocity, _smoothTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, _smoothTime);
+        }
+        
+        private Vector3 FollowingPointPosition()
+        {
+            Vector3 targetPosition = _target.position;
+            targetPosition.y += _offSetY;
+            return targetPosition;
+        }
+    }
+}
